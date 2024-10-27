c6e6e307a1280a49fc8630d0846cc25898683edf
ugm2 <unaigaraymaestre@gmail.com>
2024-10-27 20:33:10 +0100

    Adds email fetcher functionality
    Signed-off-by: ugm2 <unaigaraymaestre@gmail.com>

---
+++ src/emails/auth.py
@@ -0,0 +1,55 @@
+import logging
+import os
+import pickle
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from rich.logging import RichHandler
+
+LOGGER_LEVEL = os.getenv("LOGGER_LEVEL", "WARNING")
+logging.basicConfig(level=LOGGER_LEVEL, format="%(message)s", handlers=[RichHandler()])
+logger = logging.getLogger("LocalAuth")
+
+class LocalAuth:
+    def __init__(
+        self,
+        user_credentials: str = "secrets/user_token.pickle",
+        app_credentials: str = "secrets/app_credentials.json"
+    ):
+        self.user_credentials = user_credentials
+        self.app_credentials = app_credentials
+        self.service = self.get_service()
+
+    def get_service(self):
+        creds = self._read_token(self.user_credentials)
+
+        if not creds or not creds.valid:
+            try:
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+            except Exception as e:
+                logger.warning(f"Failed to refresh credentials: {e}")
+                creds = None
+
+            if not creds:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.app_credentials,
+                    ["https://www.googleapis.com/auth/gmail.modify"],
+                )
+                creds = flow.run_local_server(port=0)
+                self._write_token(creds, self.user_credentials)
+
+        return build("gmail", "v1", credentials=creds)
+
+    def _read_token(self, blob_name: str):
+        """Reads token from a local file."""
+        if os.path.exists(blob_name):
+            with open(blob_name, "rb") as token:
+                return pickle.load(token)
+        return None
+
+    def _write_token(self, creds, blob_name: str):
+        """Writes token to a local file."""
+        with open(blob_name, "wb") as token:
+            pickle.dump(creds, token)
---
+++ src/emails/email_fetcher.py
@@ -0,0 +1,146 @@
+import base64
+import logging
+import os
+from typing import Callable, Dict, List
+
+from rich.logging import RichHandler
+
+from src.emails.auth import LocalAuth
+
+# Get the logger level from environment variables. Default to WARNING if not set.
+LOGGER_LEVEL = os.getenv("LOGGER_LEVEL", "WARNING")
+logging.basicConfig(level=LOGGER_LEVEL, format="%(message)s", handlers=[RichHandler()])
+logger = logging.getLogger("EmailFetcher")
+
+
+class EmailFetcher:
+    """
+    A class to interact with the Gmail API, allowing fetching and parsing of emails.
+    """
+
+    def __init__(
+        self,
+        user_credentials: str = "secrets/user_token.pickle",
+        app_credentials: str = "secrets/app_credentials.json",
+    ):
+        """
+        Constructs all the necessary attributes for the EmailFetcher object.
+
+        Args:
+            user_credentials (str): Path to the token pickle file.
+            app_credentials (str): Path to the client secret JSON file.
+        """
+        self.auth = LocalAuth(user_credentials, app_credentials)
+        self.service = self.auth.service
+
+    def fetch_emails(self, query: str, max_results: int = None) -> List[Dict]:
+        """
+        Fetches emails based on a specific query.
+
+        Args:
+            query: The query to fetch the emails.
+            max_results: The maximum number of emails to fetch.
+
+        Returns:
+            A list of messages.
+        """
+        all_emails = []
+        request = self.service.users().messages().list(userId="me", q=query)
+
+        while request is not None:
+            response = request.execute()
+            logger.log(1, f"Response: {response}")
+            all_emails.extend(response.get("messages", []))
+            logger.info(f"Fetched {len(all_emails)} emails so far.")
+
+            # If max_results is specified and we've fetched enough emails, break the loop.
+            if max_results is not None and len(all_emails) >= max_results:
+                logger.info("Breaking due to max_results")
+                break
+
+            request = self.service.users().messages().list_next(request, response)
+            logger.log(1, f"Next request: {request}")
+
+        return all_emails[:max_results]
+
+    def get_email_data(self, email_id: str) -> Dict:
+        """
+        Gets data of a specific email.
+
+        Args:
+            email_id: The email ID.
+
+        Returns:
+            The email data.
+        """
+        logger.log(1, f"Fetching data for email ID: {email_id}")
+        return self.service.users().messages().get(userId="me", id=email_id).execute()
+
+    def fetch_labels(self) -> List[Dict]:
+        """
+        Fetches all labels from the user's Gmail account.
+
+        Returns:
+            The list of labels.
+        """
+        logger.info("Fetching labels from Gmail account.")
+        return (
+            self.service.users().labels().list(userId="me").execute().get("labels", [])
+        )
+
+    def get_label_id(self, label_name: str) -> str:
+        """
+        Translates a label name to its corresponding ID.
+
+        Args:
+            label_name: The label name.
+
+        Returns:
+            The label ID.
+
+        Raises:
+            ValueError: If no label is found with the given name.
+        """
+        labels = self.fetch_labels()
+        for label in labels:
+            if label["name"] == label_name:
+                logger.info(f"Found label ID for {label_name}.")
+                return label["id"]
+        logger.error(f"No label found with the name {label_name}")
+        raise ValueError(f"No label found with the name {label_name}")
+
+    def get_body(self, email: Dict) -> str:
+        """
+        Gets the body of the email.
+
+        Args:
+            email: The email data.
+
+        Returns:
+            The email body.
+        """
+        parts = email["payload"].get("parts")
+        data = parts[0]["body"]["data"] if parts else email["payload"]["body"]["data"]
+        logger.info("Decoding email body.")
+        return base64.urlsafe_b64decode(data).decode("utf-8")
+
+    def get_articles_from_emails(
+        self, emails: List[Dict], content_parser: Callable
+    ) -> List:
+        """
+        Extracts articles from a list of emails using a specific content parser.
+
+        Args:
+            emails: The list of emails.
+            content_parser: The function to parse the content.
+
+        Returns:
+            A list of articles.
+        """
+        articles = []
+        for email in emails:
+            email_data = self.get_email_data(email["id"])
+            content = self.get_body(email_data)
+            articles += content_parser(content)
+        logger.info(f"Extracted {len(articles)} articles from emails.")
+        return articles
